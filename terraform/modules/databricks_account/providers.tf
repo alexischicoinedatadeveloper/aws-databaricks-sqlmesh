9c6c5e9b2829afa7a2ec44093d3dba9516fa6f64
Alexis Chicoine <alexischicoinedatadeveloper@gmail.com>
2024-04-16 09:42:26 -0400

    database created with public networking access

--- terraform/modules/databricks_account/providers.tf
+++ terraform/modules/databricks_account/providers.tf
@@ -10,17 +10,8 @@ terraform {
   }
 }
 
-provider "aws" {
-  region     = var.region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-}
+provider "aws" {}
 
-// initialize provider in "MWS" mode to provision new workspace
 provider "databricks" {
-  alias         = "mws"
-  host          = "https://accounts.cloud.databricks.com"
-  account_id    = var.databricks_account_id
-  client_id     = var.databricks_terraform_account_client_id
-  client_secret = var.databricks_terraform_account_secret
+  alias = "mws"
 }
